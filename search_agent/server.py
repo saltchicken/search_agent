1afd7bca944048098713f127c49c10a92c45e0ef
saltchicken <john.eicher89@gmail.com>
2026-05-25 12:07:51 -0700

    Add a2a framework

---
+++ search_agent/server.py
@@ -0,0 +1,15 @@
+import uvicorn
+from google.adk.a2a.utils.agent_to_a2a import to_a2a
+
+from search_agent.agent import root_agent
+
+a2a_app = to_a2a(root_agent)
+
+def main():
+    # It is standard practice to run A2A remote agents on port 8001 
+    # locally to avoid conflicting with your primary client agents on 8000.
+    uvicorn.run(a2a_app, host="0.0.0.0", port=8001)
+
+
+if __name__ == "__main__":
+    main()
